7b76dce661d871ea92f70f7d27c156a75c79ed42
Abdulrozak <muhammaddhani494@gmail.com>
2024-10-17 08:18:38 +0700

    solve CRED Coins problem

---
+++ codechef/500_to_1000_difficulty_rating/cred_coins.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	
+	for(int i = 0; i < T; i++){
+	    int X, Y;
+	    cin >> X >> Y;
+	    
+	    cout << X*Y / 100 << endl;
+	}
+}
