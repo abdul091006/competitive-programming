eac1223bc686ca507c3e9e2388698e595fcbbeb3
Abdulrozak <muhammaddhani494@gmail.com>
2024-10-19 11:52:32 +0700

    solve Chef and Candies problem

---
+++ codechef/500_to_1000_difficulty_rating/chef_and_candies.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	
+	for(int i = 0; i < T; i++){
+	    double N, X;
+	    cin >> N >> X;
+	    
+	    if(N > X){
+	        cout << ceil((N - X) / 4) << endl;
+	    } else{
+	        cout << 0 << endl;
+	    }
+	}
+}
