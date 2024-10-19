2e119eea9fd4a0fd637b76bc3dfb12a0e77ce41c
Abdulrozak <muhammaddhani494@gmail.com>
2024-10-19 12:00:01 +0700

    solve Car or Bike problem

---
+++ codechef/500_to_1000_difficulty_rating/car_or_bike.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	
+	for(int i = 0 ; i < T; i++){
+	    int X, Y;
+	    cin >> X >> Y;
+	    
+	    if(X < Y){
+	        cout << "BIKE" << endl;
+	    }else if(X > Y){
+	        cout << "CAR" << endl;
+	    }else {
+	        cout << "SAME" << endl;
+	    }
+	}
+}
